c98e8ddd5a6a7c4d59bbb9f2cdab5a678bbebe17
Nicky Marino <nsmarino96@gmail.com>
2016-06-21 20:22:41 -0400

    Add command/response framework

--- bot.js
+++ bot.js
@@ -10,41 +10,36 @@ function respond() {
     // User post
     var request = JSON.parse(this.req.chunks[0]);
 
+    /* Check that request calls for bot response */ 
     // Has text
     if (request.text) {
         // Convert to lowercase and remove extra spaces
         var text = request.text.toLowerCase().trim();
-        console.log("Request: " + request);
+
+        // Record request
+        console.log("Message received: " + text);
 
         // Has 'jarvis'
         var index = text.indexOf("jarvis");
         if (index > -1) {
+            // Chop off jarvis from text
+            var command = text.substring(index).trim();
+
+            // Get response and post it
+            response = getMessage(command);
             this.res.writeHead(200);
-            postMessage("You rang?");
             this.res.end();
         }
         else {
             console.log("Request didn't say my name!")
+            this.res.writeHead(200);
+            this.res.end();
         }
     } else {
-        console.log("Request did not have text.");
+        console.log("Request did not have text");
         this.res.writeHead(200);
         this.res.end();
     }
-
-    /*
-    //if (request.text && (request.text.indexOf("/cool guy") > -1)) {
-        this.res.writeHead(200);
-        var response = cool();
-        postMessage(response);
-        this.res.end();
-    // ''
-    } else {
-        console.log("don't care");
-        this.res.writeHead(200);
-        this.res.end();
-    }
-    */
 }
 
 // Post message to GroupMe
@@ -81,6 +76,31 @@ function postMessage(botResponse) {
     console.log('timeout posting message '  + JSON.stringify(err));
     });
     botReq.end(JSON.stringify(body));
+}
+
+function getMessage(command) {
+    // Convert command to lowercase and trim
+    command = commandtoLowerCase().trim();
+    var response = "";
+
+    // 'help'
+    if (command.indexOf("help") > -1) {
+        response = "I'm sorry, but I can't do that right now";
+    // 'weather'
+    } else if (command.indexOf("weather") > -1) {
+        response = "It should be sunny right now, but because it's Indiana I am unsure";
+    // 'cool'
+    } else if (command.indexOf("cool") > -1) {
+        // 'alex'
+        if (command.indexOf("alex") > -1) {
+            response = "Of course not!";
+        } else {
+            response = "It's 98% likely, yes";
+        }
+    // Unknown command
+    } else {
+        response = "I'm very sorry, but I didn't understand what you said";
+    }
 }
 
 
